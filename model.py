0793e5d9e7c74df6ed9711864ea5d11aa24d281a
Markus Enzweiler <markus.enzweiler@hs-esslingen.de>
2024-01-02 00:18:37 +0100

    removed linear proj

--- model.py
+++ model.py
@@ -62,44 +62,35 @@ class Generator(BaseNet):
         img_input_shape = (num_img_channels, 28, 28)
 
         # half the number of filters in each layer
-        self.num_filters_1 = self.max_num_filters
-        self.num_filters_2 = self.num_filters_1 // 2
-        self.num_filters_3 = self.num_filters_2 // 2
+        num_filters_1 = max_num_filters
+        num_filters_2 = num_filters_1 // 2
+        num_filters_3 = num_filters_2 // 2
 
-        # we push the latent vector through a linear layer to project it
-        # to a higher dimensional space
-        self.lat_proj = nn.Linear(self.num_latent_dims, self.num_filters_1)
-         
-        # we assume input shape of (num_filters_1, 1, 1)
+                
+        # we assume input shape of (num_latent_dims, 1, 1)
 
         # Output num_filters_1 x 4 x 4
-        self.conv1 = nn.ConvTranspose2d(self.num_filters_1,   self.num_filters_1,    kernel_size=4, 
+        self.conv1 = nn.ConvTranspose2d(num_latent_dims, num_filters_1,    kernel_size=4, 
             stride=1, padding=0, bias=False)
         # Output num_filters_2 x 7 x 7   
-        self.conv2 = nn.ConvTranspose2d(self.num_filters_1,   self.num_filters_2,    kernel_size=3, 
+        self.conv2 = nn.ConvTranspose2d(num_filters_1,   num_filters_2,    kernel_size=3, 
             stride=2, padding=1, bias=False)
         # Output num_filters_3 x 14 x 14   
-        self.conv3 = nn.ConvTranspose2d(self.num_filters_2,   self.num_filters_3,    kernel_size=4, 
+        self.conv3 = nn.ConvTranspose2d(num_filters_2,   num_filters_3,    kernel_size=4, 
             stride=2, padding=1, bias=False)
         # Output num_filters_4 x 28 x 28
-        self.conv4 = nn.ConvTranspose2d(self.num_filters_3,   self.num_img_channels, kernel_size=4, 
+        self.conv4 = nn.ConvTranspose2d(num_filters_3,   num_img_channels, kernel_size=4, 
             stride=2, padding=1, bias=False)
        
         # Batch normalization
-        self.bn1 = nn.BatchNorm2d(self.num_filters_1)
-        self.bn2 = nn.BatchNorm2d(self.num_filters_2)
-        self.bn3 = nn.BatchNorm2d(self.num_filters_3)      
+        self.bn1 = nn.BatchNorm2d(num_filters_1)
+        self.bn2 = nn.BatchNorm2d(num_filters_2)
+        self.bn3 = nn.BatchNorm2d(num_filters_3)      
     
-    def forward(self, x):
-
-        # Assuming x has a shape of [batch_size, num_latent_dims, 1, 1]
-        # Flatten x to the shape [batch_size, num_latent_dims]
-        x = x.view(x.size(0), -1)
-        # Pass it through the linear layer
-        x = self.lat_proj(x) 
-        # reshape the latent vector to a 4D tensor of shape (batch_size, num_filters_1, 1, 1)
+    def forward(self, x):        
+        # reshape the latent vector to a 4D tensor of shape (batch_size, num_latent_dims, 1, 1)
   
-        x = x.view(len(x), self.num_filters_1, 1, 1)    
+        x = x.view(len(x), self.num_latent_dims, 1, 1)    
         
         # push it through the layers
         x = F.relu(self.bn1(self.conv1(x)))
